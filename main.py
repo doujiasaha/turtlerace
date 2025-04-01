0eb12655c43f7d65b09464bf96dcce5b194d9da2
Asakura <Asakura-MBA>
2025-04-01 10:49:11 +0200

    Initial WASD movement

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+north = 90
+
+def move_forwards():
+    tim.forward(30)
+
+def turn_right():
+    tim.right(30)
+    
+def turn_left():
+    tim.left(30)
+    
+def move_backwards():
+    tim.back(30)
+
+tim.seth(90)
+
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.exitonclick()
