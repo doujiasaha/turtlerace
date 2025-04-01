1844c166f8ea85990caa8a070bb832a1f4ecbd8c
Asakura <Asakura-MBA>
2025-04-01 18:06:47 +0200

    help

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+class New_turtle():
+    def __init__(self, id):
+        self.id = id   
+        #possible colors
+        color = ["red","blue","green","yellow","black","pink",]
+        
+        self.turtle = Turtle()
+        self.turtle.color(color[self.id])
+        self.turtle.shape("turtle")
+        self.turtle.penup()
+        
+        self.starting_position()
+    
+        
+    def starting_position(self):
+        
+        y_positions = [-150,-100,-50,0,50,100]
+        self.turtle.goto(x=-230,y=y_positions[self.id])    
+       
+
 screen = Screen()
+screen.title("Welcome to the turtlerace!")
+screen.setup(width=500, height=400)
 
-north = 90
+user_bet = screen.textinput("Which turtle will win?", prompt="The candidates are: red, blue, green, yellow, black, pink")
+objects = [New_turtle(i) for i in range(6)]
 
-def move_forwards():
-    tim.forward(30)
+is_race_on = False
+if user_bet:
+    is_race_on = True
 
-def turn_right():
-    tim.right(30)
-    
-def turn_left():
-    tim.left(30)
-    
-def move_backwards():
-    tim.back(30)
+while is_race_on:
+    for turtle in objects:
+        if turtle.turtle.xcor() > 230:
+            winning_color = turtle.turtle.pencolor()
+            is_race_on = False
+            if winning_color == user_bet:
+                print("You've won")
+            else:
+                print(f"The winning turtle was {winning_color}")
+            break
+        turtle.turtle.forward(random.randint(1,20))
+        
 
-tim.seth(90)
 
 
-screen.listen()
-screen.onkey(move_forwards, "w")
-screen.onkey(move_backwards, "s")
-screen.onkey(turn_left, "a")
-screen.onkey(turn_right, "d")
+        
 screen.exitonclick()
